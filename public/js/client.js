92e923e380c6eb8d9eb3f42d81dd9008397e25f8
DphnZwp <zwuupdaphne@gmail.com>
2022-04-14 01:05:18 +0200

    post form with loading message

---
+++ public/js/client.js
@@ -0,0 +1,35 @@
+const loadingButton = document.querySelector('#loading-button')
+const loadingMessage = document.querySelector('.loading-text')
+// POST variables
+const apiBase = 'https://codingthecurbs.api.fdnd.nl/v1/smartzone'
+const smartzonePostForm = document.querySelector('form#smartzonePostForm')
+
+loadingButton.addEventListener('click', () => {
+      loadingMessage.classList.add('loading-show')
+  }
+ )
+
+ smartzonePostForm.addEventListener('submit', (post) => {
+   post.preventDefault()
+ 
+   let data = {
+     smartzoneId: document.querySelector('#smartzoneId').value,
+     name: document.querySelector('#name').value,
+     town: document.querySelector('#town').value,
+     location: document.querySelector('#location').value,
+     function: document.querySelector('#function').value,
+     time: document.querySelector('#time').value,
+     size: document.querySelector('#size').value,
+     utilization: document.querySelector('#utilization').value,
+     description: document.querySelector('#description').value,
+     image: document.querySelector('#image').value,
+   }
+ 
+   fetch(apiBase, {
+     method: 'POST',
+     headers: {
+       'Content-Type': 'application/json',
+     },
+     body: JSON.stringify(data),
+   })
+ })
