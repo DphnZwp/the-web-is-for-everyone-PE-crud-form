0b6c1d8f0e5d306d629b6c963cbb4e6db830c78f
DphnZwp <zwuupdaphne@gmail.com>
2022-04-14 01:43:24 +0200

    Post foutmelding opgelost
    Ik heb alleen het probleem dat hij dubbel post in plaats van een keer wanneer je op submit drukt.

--- public/js/client.js
+++ public/js/client.js
@@ -2,34 +2,31 @@ const loadingButton = document.querySelector('#loading-button')
 const loadingMessage = document.querySelector('.loading-text')
 // POST variables
 const apiBase = 'https://codingthecurbs.api.fdnd.nl/v1/smartzone'
-// const smartzonePostForm = document.querySelector('form#smartzonePostForm')
+const smartzonePostForm = document.querySelector('#smartzonePostForm')
 
 loadingButton.addEventListener('click', () => {
-      loadingMessage.classList.add('loading-show')
-  }
- )
+   loadingMessage.classList.add('loading-show')
+})
 
- smartzonePostForm.addEventListener('submit', (post) => {
-   post.preventDefault()
- 
-   let data = {
-     smartzoneId: document.querySelector('#smartzoneId').value,
-     name: document.querySelector('#name').value,
-     town: document.querySelector('#town').value,
-     location: document.querySelector('#location').value,
-     function: document.querySelector('#function').value,
-     time: document.querySelector('#time').value,
-     size: document.querySelector('#size').value,
-     utilization: document.querySelector('#utilization').value,
-     description: document.querySelector('#description').value,
-     image: document.querySelector('#image').value,
-   }
- 
+smartzonePostForm.addEventListener('submit', () => {
+
+let data = {
+   name: document.querySelector('#name').value,
+   town: document.querySelector('#town').value,
+   location: document.querySelector('#location').value,
+   function: document.querySelector('#function').value,
+   time: document.querySelector('#time').value,
+   size: document.querySelector('#size').value,
+   utilization: document.querySelector('#utilization').value,
+   description: document.querySelector('#description').value,
+   image: document.querySelector('#image').value,
+}
+
    fetch(apiBase, {
-     method: 'POST',
-     headers: {
-       'Content-Type': 'application/json',
-     },
-     body: JSON.stringify(data),
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
    })
- })
+})
